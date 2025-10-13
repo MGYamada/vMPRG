d9cc8e5dbad8b5ab04274811a53d4813a7a80f1e
Masahiko Yamada <masa@Mac.lan>
2025-10-13 17:27:56 +0900

    v0.1.2 2d

--- 2d/SquareHeisenberg.jl
+++ 2d/SquareHeisenberg.jl
@@ -16,18 +16,57 @@ function proj!(Q, R)
     R .= (R .+ conj.(ein"ijk, k -> jik"(R, [1.0, -1.0, 1.0]))) ./ 2
 end
 
-function Optim.retract!(::PEPS2, qr)
-    Q = reshape(qr[1 : χ₂ ^ 4, 1], χ₂, χ₂, χ₂, χ₂) .+ im * reshape(qr[1 : χ₂ ^ 4, 2], χ₂, χ₂, χ₂, χ₂)
-    R = reshape(qr[χ₂ ^ 4 + 1 : end, 1], χ₂, χ₂, D - 1) .+ im * reshape(qr[χ₂ ^ 4 + 1 : end, 2], χ₂, χ₂, D - 1)
+function Optim.retract!(mfd::PEPS2, qr)
+    Q = reshape(qr[1 : χ₂ ^ 4], χ₂, χ₂, χ₂, χ₂)
+    R = reshape(qr[χ₂ ^ 4 + 1 : end], χ₂, χ₂, D - 1)
     proj!(Q, R)
-    qr .= (x -> hcat(real.(x), imag.(x)))(vcat(vec(Q), vec(R)))
+    qr .= vcat(vec(Q), vec(R))
 end
 
-function Optim.project_tangent!(::PEPS2, dqr, qr)
-    Q = reshape(dqr[1 : χ₂ ^ 4, 1], χ₂, χ₂, χ₂, χ₂) .+ im * reshape(dqr[1 : χ₂ ^ 4, 2], χ₂, χ₂, χ₂, χ₂)
-    R = reshape(dqr[χ₂ ^ 4 + 1 : end, 1], χ₂, χ₂, D - 1) .+ im * reshape(dqr[χ₂ ^ 4 + 1 : end, 2], χ₂, χ₂, D - 1)
+function Optim.project_tangent!(mfd::PEPS2, dqr, qr)
+    Q = reshape(dqr[1 : χ₂ ^ 4], χ₂, χ₂, χ₂, χ₂)
+    R = reshape(dqr[χ₂ ^ 4 + 1 : end], χ₂, χ₂, D - 1)
     proj!(Q, R)
-    dqr .= (x -> hcat(real.(x), imag.(x)))(vcat(vec(Q), vec(R)))
+    dqr .= vcat(vec(Q), vec(R))
+end
+
+struct ThreeLegLadder
+    M::Array{ComplexF64, 3}
+    Q::Array{ComplexF64, 4}
+    R::Array{ComplexF64, 3}
+end
+
+struct TwoLegLadder
+    Q::Array{ComplexF64, 4}
+    R::Array{ComplexF64, 3}
+end
+
+function SuperVUMPS.local_energy(AL, AC, h::ThreeLegLadder)
+    χ₁, = size(AL)
+    χ₂, = size(h.Q)
+    l1 = reshape(AL, χ₁, χ₂, :, χ₂, χ₁)
+    l2 = ein"ijklm, inklo -> jmno"(conj.(l1), l1)
+    l3 = ein"ijklm, ijnlo -> kmno"(conj.(l1), l1)
+    l4 = ein"ijklm, ijkno -> lmno"(conj.(l1), l1)
+    r1 = reshape(AC, χ₁, χ₂, :, χ₂, χ₁)
+    r2 = ein"ijklm, noklm -> ijno"(conj.(r1), r1)
+    r3 = ein"ijklm, njolm -> ikno"(conj.(r1), r1)
+    r4 = ein"ijklm, njkom -> ilno"(conj.(r1), r1)
+    -(ein"imkn, (ijkl, jmln) -> "(conj.(h.Q), l2, r2)[] + ein"ikx, (mnx, (ijkl, jmln)) -> "(h.M, h.M, l3, r3)[] + ein"imkn, (ijkl, jmln) -> "(h.Q, l4, r4)[] +
+    ein"jnx, (kox, (ijklm, inolm)) -> "(conj.(h.R), h.M, conj.(r1), r1)[] + ein"knx, (lox, (ijklm, ijnom)) -> "(h.M, h.R, conj.(r1), r1)[])
+end
+
+function SuperVUMPS.local_energy(AL, AC, h::TwoLegLadder)
+    χ₁, = size(AL)
+    χ₂, = size(h.Q)
+    l1 = reshape(AL, χ₁, χ₂, χ₂, χ₁)
+    l2 = ein"ijkl, imkn -> jlmn"(conj.(l1), l1)
+    l3 = ein"ijkl, ijmn -> klmn"(conj.(l1), l1)
+    r1 = reshape(AC, χ₁, χ₂, χ₂, χ₁)
+    r2 = ein"ijkl, mnkl -> ijmn"(conj.(r1), r1)
+    r3 = ein"ijkl, mjnl -> ikmn"(conj.(r1), r1)
+    -(ein"imkn, (ijkl, jmln) -> "(conj.(h.Q), l2, r2)[] + ein"imkn, (ijkl, jmln) -> "(h.Q, l3, r3)[] +
+    ein"jmx, (knx, (ijkl, imnl)) -> "(conj.(h.R), h.R, conj.(r1), r1)[])
 end
 
 function main()
@@ -43,35 +82,21 @@ function main()
     R = randn(ComplexF64, χ₂, χ₂, D - 1)
     proj!(Q, R)
 
+    A3 = canonicalMPS(ComplexF64, χ₁, d * χ₂ ^ 2)
+    A2 = canonicalMPS(ComplexF64, χ₁, χ₂ ^ 2)
+
     println("========================")
     println("χ = (χ₁, χ₂) = ", (χ₁, χ₂))
     println("========================")
 
-    A3 = canonicalMPS(ComplexF64, χ₁, d * χ₂ ^ 2)
-    A2 = canonicalMPS(ComplexF64, χ₁, χ₂ ^ 2)
-
     function fg!(F, G, x)
         E, (dx,) = withgradient(x) do x
-            Q = reshape(x[1 : χ₂ ^ 4, 1], χ₂, χ₂, χ₂, χ₂) .+ im * reshape(x[1 : χ₂ ^ 4, 2], χ₂, χ₂, χ₂, χ₂)
-            R = reshape(x[χ₂ ^ 4 + 1 : end, 1], χ₂, χ₂, D - 1) .+ im * reshape(x[χ₂ ^ 4 + 1 : end, 2], χ₂, χ₂, D - 1)
-
-            h3 = -(reshape(ein"abcd, ik, jl, αγ, βδ -> aiαbjβckγdlδ"(conj.(Q), Matrix{Float64}(I, d, d), Matrix{Float64}(I, d, d), Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂)), d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2) .+
-            reshape(ein"ac, bd, ik, jl, αβγδ -> aiαbjβckγdlδ"(Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, d, d), Matrix{Float64}(I, d, d), Q), d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2) .+
-            ein"ik, jl -> ijkl"(reshape(ein"(ijx, klx), mn -> ikmjln"(conj.(R), M, Matrix{Float64}(I, χ₂, χ₂)), d * χ₂ ^ 2, d * χ₂ ^ 2), Matrix{Float64}(I, d * χ₂ ^ 2, d * χ₂ ^ 2)) ./ 2 .+
-            ein"ik, jl -> ijkl"(reshape(ein"ij, (klx, mnx) -> ikmjln"(Matrix{Float64}(I, χ₂, χ₂), M, R), d * χ₂ ^ 2, d * χ₂ ^ 2), Matrix{Float64}(I, d * χ₂ ^ 2, d * χ₂ ^ 2)) ./ 2 .+
-            ein"ik, jl -> ijkl"(Matrix{Float64}(I, d * χ₂ ^ 2, d * χ₂ ^ 2), reshape(ein"(ijx, klx), mn -> ikmjln"(conj.(R), M, Matrix{Float64}(I, χ₂, χ₂)), d * χ₂ ^ 2, d * χ₂ ^ 2)) ./ 2 .+
-            ein"ik, jl -> ijkl"(Matrix{Float64}(I, d * χ₂ ^ 2, d * χ₂ ^ 2), reshape(ein"ij, (klx, mnx) -> ikmjln"(Matrix{Float64}(I, χ₂, χ₂), M, R), d * χ₂ ^ 2, d * χ₂ ^ 2)) ./ 2 .+
-            reshape(ein"ac, bd, (ikx, jlx), αγ, βδ -> aiαbjβckγdlδ"(Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂), M, M, Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂)), d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2, d * χ₂ ^ 2))
-            E3, A3 = svumps(h3, A3)
-
-            h2 = -(reshape(ein"abcd, αγ, βδ -> aαbβcγdδ"(conj.(Q), Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂)), χ₂ ^ 2, χ₂ ^ 2, χ₂ ^ 2, χ₂ ^ 2) .+
-            reshape(ein"ac, bd, αβγδ -> aαbβcγdδ"(Matrix{Float64}(I, χ₂, χ₂), Matrix{Float64}(I, χ₂, χ₂), Q), χ₂ ^ 2, χ₂ ^ 2, χ₂ ^ 2, χ₂ ^ 2) .+
-            ein"ik, jl -> ijkl"(reshape(ein"ijx, klx -> ikjl"(conj.(R), R), χ₂ ^ 2, χ₂ ^ 2), Matrix{Float64}(I, χ₂ ^ 2, χ₂ ^ 2)) ./ 2 .+
-            ein"ik, jl -> ijkl"(Matrix{Float64}(I, χ₂ ^ 2, χ₂ ^ 2), reshape(ein"ijx, klx -> ikjl"(conj.(R), R), χ₂ ^ 2, χ₂ ^ 2)) ./ 2)
-            E2, A2 = svumps(h2, A2)
+            Q0 = reshape(x[1 : χ₂ ^ 4], χ₂, χ₂, χ₂, χ₂) 
+            R0 = reshape(x[χ₂ ^ 4 + 1 : end], χ₂, χ₂, D - 1)
+            E3, A3 = svumps(ThreeLegLadder(M, Q0, R0), A3)
+            E2, A2 = svumps(TwoLegLadder(Q0, R0), A2)
             E3 - E2
         end
-
         if G !== nothing
             G .= dx
         end
@@ -80,7 +105,11 @@ function main()
         end
     end
 
-    res = optimize(Optim.only_fg!((x, y, z) -> fg!(x, y, z)), (x -> hcat(real.(x), imag.(x)))(vcat(vec(Q), vec(R))), LBFGS(manifold = PEPS2()), Optim.Options(f_tol = 1e-8, allow_f_increases = true, show_trace = true))
+    res = optimize(Optim.only_fg!((x, y, z) -> fg!(x, y, z)), vcat(vec(Q), vec(R)), LBFGS(manifold = PEPS2()), Optim.Options(f_reltol = 1e-8, allow_f_increases = true, show_trace = true))
+    x = Optim.minimizer(res)
+    Q .= reshape(x[1 : χ₂ ^ 4], χ₂, χ₂, χ₂, χ₂)
+    R .= reshape(x[χ₂ ^ 4 + 1 : end], χ₂, χ₂, D - 1)
+
     println(res)
 end
 
